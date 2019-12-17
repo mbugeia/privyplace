78ddc00faee34e9ce60668f69d07f2f81ba894ed
Maxime Bugeia <maxime.bugeia@gmail.com>
2019-12-17 17:36:33 +0100

    Allow no push in build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,8 @@
 set -eu
 DOCKER_ID_USER=${DOCKER_ID_USER:-"privyplace"}
 base_image_path="$1"
-tag="${2:-latest}"
+tag="${2:-"latest"}"
+push="${3:-"yes"}"
 
 for image_path in $(find $base_image_path -type d)
 do
@@ -15,9 +16,15 @@ do
   then
     docker build -t "$DOCKER_ID_USER"/"$image":latest "$image_path" --no-cache
     docker tag "$DOCKER_ID_USER"/"$image":latest "$DOCKER_ID_USER"/"$image":"$tag"
-    docker push "$DOCKER_ID_USER"/"$image":"$tag" 
+    if [ "$push" = "yes" ]
+    then
+      docker push "$DOCKER_ID_USER"/"$image":"$tag" 
+    fi
   else
     docker build -t "$DOCKER_ID_USER"/"$image":latest "$image_path"
   fi
-  docker push "$DOCKER_ID_USER"/"$image":latest
+  if [ "$push" = "yes" ]
+  then
+    docker push "$DOCKER_ID_USER"/"$image":latest
+  fi
 done
